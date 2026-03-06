083fa528a9cf7bd3f2dc8238c5e4d50384c2804c
omedcodes <omedsafi21@gmail.com>
2026-03-06 16:51:03 +0100

    Add event loop and increase square by one each frame

--- raycast.c
+++ raycast.c
@@ -1,13 +1,37 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
-int main(int argc, char *argv[])
-{
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Raycasting Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Delay(3000);
-    SDL_Quit();
-    return 0;
+#define COLOR_WHITE 0xFFFFFFFF
+
+int main(int argc, char *argv[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("Raycasting Engine", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+  bool running = true;
+  int counter = 0;
+  while (running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        running = false;
+        break;
+      }
+    }
+    SDL_Rect rect = {WIDTH / 2, HEIGHT / 2, 100 + counter++, 100 + counter++};
+    SDL_FillRect(surface, &rect, COLOR_WHITE);
+
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(10);
+  }
+
+  SDL_Quit();
+  return 0;
 }
